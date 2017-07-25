33f4bf00b3fa297c56ab439d792a0dbdaa185caf
Sven Bursch-Osewold <sb_git@bursch.com>
2017-07-25 08:13:45 +0200

    Adding d1-mini

--- install.sh
+++ install.sh
@@ -21,7 +21,11 @@ arduino --install-library USBHost
 # Default to NodeMCU V2
 arduino --board esp8266:esp8266:nodemcuv2 --pref build.path=$HOME/mybuild --pref "compiler.warning_level=default" --save-prefs 
 
-# Fixing a Problem with the boards.txt -- choosing 4MB /3MB configuration
+# Fixing a Problem with the boards.txt
+# ld: cannot open linker script file {build.flash_ld}: No such file or directory
+# choosing "4M (3M SPIFFS)" Konfiguration
+
 sed -i.bak 's/nodemcu.menu.FlashSize.4M3M/nodemcu/g' $HOME/.arduino15/packages/esp8266/hardware/esp8266/2.3.0/boards.txt
 sed -i.bak 's/nodemcuv2.menu.FlashSize.4M3M/nodemcuv2/g' $HOME/.arduino15/packages/esp8266/hardware/esp8266/2.3.0/boards.txt
+sed -i.bak 's/d1_mini.menu.FlashSize.4M3M/d1_mini/g' $HOME/.arduino15/packages/esp8266/hardware/esp8266/2.3.0/boards.txt
 
